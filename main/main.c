4ef62eb14a82ea55266f0fa01f940d83ef6014b3
nlemoing <nick@lemoing.ca>
2023-02-25 22:59:59 -0500

    Add wifi

--- main/main.c
+++ main/main.c
@@ -5,6 +5,7 @@
 #include "esp_bt.h"
 #include "esp_err.h"
 #include "esp_http_client.h"
+#include "esp_wifi.h"
 #include "nvs.h"
 #include "nvs_flash.h"
 #include <string.h>
@@ -13,6 +14,7 @@
 #include "freertos/event_groups.h"
 
 #include "usb_hid_codes.h"
+#include "wifi_constants.h"
 
 // Set this to the BT MAC adress of the HID device that you're connecting to
 static esp_bd_addr_t _peer_bd_addr = { 0xDC, 0x2C, 0x26, 0x00, 0x37, 0xA9 };
@@ -25,8 +27,12 @@ static EventGroupHandle_t _hid_event_group = NULL;
 #define HID_CONNECTED    0x02
 #define HID_CLOSED       0x04
 
+// _wifi_event_group
+static EventGroupHandle_t _wifi_event_group = NULL;
+#define WIFI_CONNECTED   0x01
+#define WIFI_FAIL        0x02
+
 // HTTP defs
-const char *host = "192.168.1.124";
 char *key_mappings[0x64];
 static void init_key_mappings(void) {
     key_mappings[KEY_KP0] = "0";
@@ -39,6 +45,7 @@ static void init_key_mappings(void) {
     key_mappings[KEY_KP7] = "7";
     key_mappings[KEY_KP8] = "8";
     key_mappings[KEY_KP9] = "9";
+    key_mappings[KEY_KPDOT] = "dot";
     key_mappings[KEY_KPSLASH] = "slash";
     key_mappings[KEY_KPASTERISK] = "asterisk";
     key_mappings[KEY_KPMINUS] = "minus";
@@ -52,7 +59,7 @@ static void init_key_mappings(void) {
 static void send_request(const char *path) {
     const char *TAG = "send_request";
     esp_http_client_config_t config = {
-        .host = host,
+        .host = SERVER_IP,
         .path = path,
     };
 
@@ -233,6 +240,91 @@ static bool waitForConnect(int timeout) {
     return false;
 }
 
+static int s_retry_num = 0;
+
+static void wifi_event_handler(void* arg, esp_event_base_t event_base,
+                                int32_t event_id, void* event_data)
+{
+    const char *TAG = "wifi_event_handler";
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < 5) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            xEventGroupSetBits(_wifi_event_group, WIFI_FAIL);
+        }
+        ESP_LOGI(TAG,"connect to the AP fail");
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+        xEventGroupSetBits(_wifi_event_group, WIFI_CONNECTED);
+    }
+}
+
+
+static bool _init_wifi(void) {
+    const char *TAG = "_init_wifi";
+
+    if (!_wifi_event_group) {
+        _wifi_event_group = xEventGroupCreate();
+    }
+
+    ESP_ERROR_CHECK(esp_netif_init());
+
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
+
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(WIFI_EVENT,
+                                                        ESP_EVENT_ANY_ID,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_any_id));
+    ESP_ERROR_CHECK(esp_event_handler_instance_register(IP_EVENT,
+                                                        IP_EVENT_STA_GOT_IP,
+                                                        &wifi_event_handler,
+                                                        NULL,
+                                                        &instance_got_ip));
+
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = WIFI_HOSTNAME,
+            .password = WIFI_PASSWORD,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA) );
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config) );
+    ESP_ERROR_CHECK(esp_wifi_start() );
+
+    ESP_LOGI(TAG, "wifi_init_sta finished.");
+
+    /* Waiting until either the connection is established (WIFI_CONNECTED_BIT) or connection failed for the maximum
+     * number of re-tries (WIFI_FAIL_BIT). The bits are set by event_handler() (see above) */
+    EventBits_t bits = xEventGroupWaitBits(_wifi_event_group,
+            WIFI_CONNECTED | WIFI_FAIL,
+            pdFALSE,
+            pdFALSE,
+            portMAX_DELAY);
+
+    /* xEventGroupWaitBits() returns the bits before the call returned, hence we can test which event actually
+     * happened. */
+    if (bits & WIFI_CONNECTED) {
+        ESP_LOGI(TAG, "Connected to WiFi network %s successfully.", WIFI_HOSTNAME);
+        return true;
+    }
+    return false;
+}
+
 void app_main(void)
 {
     const char *TAG = "app_main";
@@ -263,4 +355,9 @@ void app_main(void)
     } else {
         ESP_LOGI(TAG, "Connection unsuccessful");
     }
+
+    // if (!_init_wifi()) {
+    //     ESP_LOGE(TAG, "Failed to initialize WiFi, exiting.");
+    //     return;
+    // }
 }
